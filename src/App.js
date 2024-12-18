b787aafa5942c0c6a073451e5f3b9e3fd7255bb9
Sergey <sinzem@gmail.com>
2024-12-18 21:56:33 +0200

    app_clean

--- src/App.js
+++ src/App.js
@@ -4,26 +4,11 @@ import './App.css';
 import Header from './components/Header/Header';
 import ProductList from './components/ProductList/ProductList';
 import Form from './components/Form/Form';
-// import { useTelegram } from './hooks/useTelegram';/* (с хуком не работает, обьект tg теряется) */
+
 
 function App() {
-
-  // const {tg, onToggleButton} = useTelegram();
-
+  
   const tg = window.Telegram.WebApp;
-
-  const onToggleButton = () => { /* (функция для показа/скрытия встроенной кнопки tg, используем встроенные методы(подробнее в документации)) */
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.show();
-    }
-}
-
-  useEffect(() => {
-    // tg.ready(); /* (вызываем для обозначения готовности работы TG - проинициализировалось и можно отрисовывать) */
-  }, [])
-
 
   return (
     <div className="App">
